1318182994128f75555c413ac2a9220f1b2dba0c
habibur-pro <va.habibur@gmail.com>
2024-09-28 00:03:46 +0600

    feat: added primary secondary and highlight colors

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,6 @@
 const page = () => {
   return (
-    <div className="flex items-center justify-center h-screen">
+    <div className="flex  items-center justify-center h-screen">
       <h1 className="text-2xl text-white">Coming soon..</h1>
     </div>
   );
